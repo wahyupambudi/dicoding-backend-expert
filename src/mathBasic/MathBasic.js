35c2764e53be459a44f8b957a5126452ae27692e
wahyupambudi <wahyupambudi823@gmail.com>
2023-12-04 15:14:15 +0700

    add validate for function add only accept two parameter

--- src/mathBasic/MathBasic.js
+++ src/mathBasic/MathBasic.js
@@ -1,10 +1,13 @@
 const MathBasic = {
-  add: () => {},
+  add: (...args) => {
+    // args dapat menggabungkan seluruh parameter menjadi array
+    if (args.length < 2 || args.length > 2) {
+      throw new Error("function add only accept two parameter");
+    }
+  },
   subtract: () => {},
   multiply: () => {},
   divide: () => {},
 };
-
-console.log(typeof MathBasic.add);
 
 module.exports = MathBasic;
