b0c98644a56f7ee89d85042c8d705dac008587c7
wahyupambudi <wahyupambudi823@gmail.com>
2023-12-05 13:53:04 +0700

    add more test

--- src/mathBasic/MathBasic.js
+++ src/mathBasic/MathBasic.js
@@ -1,8 +1,37 @@
+// Skenario Pengujian:
+
+// Sebuah objek MathBasic
+//     Harus memiliki fungsi add, subtract, multiply, dan divide
+//     Sebuah fungsi add:
+//         Harus mengembalikan eror ketika tidak diberikan dua parameter
+//         Harus mengembalikan eror ketika diberikan parameter selain number
+//         Harus mengembalikan nilai a + b ketika diberikan dua parameter number
+//     Sebuah fungsi subtract:
+//         Harus mengembalikan eror ketika tidak diberikan dua parameter
+//         Harus mengembalikan eror ketika diberikan parameter selain number
+//         Harus mengembalikan nilai a - b ketika diberikan dua parameter number
+//     Sebuah fungsi multiply:
+//         Harus mengembalikan eror ketika tidak diberikan dua parameter
+//         Harus mengembalikan eror ketika diberikan parameter selain number
+//         Harus mengembalikan nilai a * b ketika diberikan dua parameter number
+//     Sebuah fungsi divide:
+//         Harus mengembalikan eror ketika tidak diberikan dua parameter
+//         Harus mengembalikan eror ketika diberikan parameter selain number
+//         Harus mengembalikan nilai a / b ketika diberikan dua parameter number
+
 const MathBasic = {
   add: (...args) => {
     // args dapat menggabungkan seluruh parameter menjadi array
-    if (args.length < 2 || args.length > 2) {
+    if (args.length !== 2) {
       throw new Error("function add only accept two parameter");
+    }
+
+    // mendapatkan array ke 0 dan ke 1
+    const a = args[0];
+    const b = args[1];
+
+    if (typeof a !== "number" || typeof b !== "number") {
+      throw new Error("function only accept parameter number");
     }
   },
   subtract: () => {},
